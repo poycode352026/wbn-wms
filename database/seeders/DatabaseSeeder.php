99d80d7922c925f831250289d5da09882599a462
poycode352026 <wen.yanpeng.work@gmail.com>
2026-06-09 15:07:21 +0900

    Adding new data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,78 +27,78 @@ class DatabaseSeeder extends Seeder
             'name'              => 'Super Admin',
             'email'             => 'superadmin@gmail.com',
             'email_verified_at' => now(),
-            'password'          => Hash::make('password'),
+            'password'          => Hash::make('superadmin'),
             'role'              => 'super_admin',
             'is_active'         => true,
         ]);
 
         // ── Procurement Admin ───────────────────────────────────────────────────
         User::create([
-            'employee_id'       => '6260305002',
-            'name'              => 'Admin Procurement',
-            'email'             => 'adminpr@gmail.com',
+            'employee_id'       => '6250417003',
+            'name'              => 'Alvin Anderson',
+            'email'             => 'alvin.anderson@gmail.com',
             'email_verified_at' => now(),
-            'password'          => Hash::make('password'),
+            'password'          => Hash::make('6250417003'),
             'role'              => 'procurement_admin',
             'is_active'         => true,
         ]);
 
         // ── WH Admin ───────────────────────────────────────────────────────────
         User::create([
-            'employee_id'       => '6260305003',
-            'name'              => 'Admin Gudang',
-            'email'             => 'whadmin@gmail.com',
+            'employee_id'       => '6230923009',
+            'name'              => 'Ari Wibowo',
+            'email'             => 'ari.wibowo@gmail.com',
             'email_verified_at' => now(),
-            'password'          => Hash::make('password'),
+            'password'          => Hash::make('6230923009'),
             'role'              => 'wh_admin',
             'is_active'         => true,
         ]);
 
         // ── WH Supervisor ──────────────────────────────────────────────────────
         User::create([
-            'employee_id'       => '6260305004',
-            'name'              => 'Supervisor Gudang',
-            'email'             => 'supervisor@gmail.com',
+            'employee_id'       => '6260305002',
+            'name'              => 'Arseno',
+            'email'             => 'arsenor@gmail.com',
             'email_verified_at' => now(),
-            'password'          => Hash::make('password'),
+            'password'          => Hash::make('6260305002'),
             'role'              => 'wh_supervisor',
             'is_active'         => true,
         ]);
 
         // ── WH Manager ─────────────────────────────────────────────────────────
         User::create([
-            'employee_id'       => '6260305005',
-            'name'              => 'Manager Gudang',
-            'email'             => 'whmanager@gmail.com',
+            'employee_id'       => '6260305003',
+            'name'              => 'Bill Chen',
+            'email'             => 'bill.chen@gmail.com',
             'email_verified_at' => now(),
-            'password'          => Hash::make('password'),
+            'password'          => Hash::make('6260305003'),
             'role'              => 'wh_manager',
             'is_active'         => true,
         ]);
 
-        // // ── Admin Dept (IT) ────────────────────────────────────────────────────
-        // User::create([
-        //     'employee_id'       => '6260305006',
-        //     'name'              => 'Admin Dept IT',
-        //     'email'             => 'admindept@gmail.com',
-        //     'email_verified_at' => now(),
-        //     'password'          => Hash::make('password'),
-        //     'role'              => 'admin_dept',
-        //     'department_id'     => $deptIT->id,
-        //     'is_active'         => true,
-        // ]);
+        // ── Admin Dept (IT) ────────────────────────────────────────────────────
+        User::create([
+            'employee_id'       => '11111111',
+            'name'              => 'Frediyodi',
+            'email'             => 'admindept@gmail.com',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('password'),
+            'role'              => 'admin_dept',
+            'department_id'     => $deptIT->id,
+            'is_active'         => true,
+        ]);
 
-        // // ── Manager Dept (IT) ──────────────────────────────────────────────────
-        // User::create([
-        //     'employee_id'       => '6260305007',
-        //     'name'              => 'Manager Dept IT',
-        //     'email'             => 'managerdept@gmail.com',
-        //     'email_verified_at' => now(),
-        //     'password'          => Hash::make('password'),
-        //     'role'              => 'manager_dept',
-        //     'department_id'     => $deptIT->id,
-        //     'is_active'         => true,
-        // ]);
+        // ── Manager Dept (IT) ──────────────────────────────────────────────────
+        User::create([
+            'employee_id'       => '22222222',
+            'name'              => 'Salim Lim',
+            'email'             => 'salim.lim@gmail.com',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('22222222'),
+            'role'              => 'manager_dept',
+            'department_id'     => $deptIT->id,
+            'is_active'         => true,
+        ]);
 
         $this->call([
             WarehouseAndCategorySeeder::class,
